c84d982c49e3fb766988fcb0d2e70786f06ed91a
mabin <jansan_godsend@sina.com>
2019-02-20 14:33:25 +0800

    消费者端维护

---
+++ ShareAirCleanerSystem/AirCleaner/src/main/java/com/qt/air/cleaner/market/controller/activity/CustomerController.java
@@ -0,0 +1,5 @@
+package com.qt.air.cleaner.market.controller.activity;
+
+public class CustomerController {
+
+}
---
+++ ShareAirCleanerSystem/AirCleaner/src/main/java/com/qt/air/cleaner/market/service/activity/impl/CustomerServiceImpl.java
@@ -0,0 +1,7 @@
+package com.qt.air.cleaner.market.service.activity.impl;
+
+import com.qt.air.cleaner.market.service.activity.CustomerService;
+
+public class CustomerServiceImpl implements CustomerService {
+
+}
