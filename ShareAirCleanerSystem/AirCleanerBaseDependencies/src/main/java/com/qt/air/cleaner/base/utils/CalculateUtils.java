5dd4c15d7d213e81dbb0094ddeeb1c3e1135210f
mabin <jansan_godsend@sina.com>
2019-02-26 14:34:54 +0800

    金额计算bug修改

--- ShareAirCleanerSystem/AirCleanerBaseDependencies/src/main/java/com/qt/air/cleaner/base/utils/CalculateUtils.java
+++ ShareAirCleanerSystem/AirCleanerBaseDependencies/src/main/java/com/qt/air/cleaner/base/utils/CalculateUtils.java
@@ -13,7 +13,7 @@ public class CalculateUtils {
 	public static Float add(Float var1, Float var2) {
 		BigDecimal b1 = new BigDecimal(Float.toString(var1));
 		BigDecimal b2 = new BigDecimal(Float.toString(var2));
-		return b1.subtract(b2).setScale(2, BigDecimal.ROUND_DOWN).floatValue();
+		return b1.add(b2).setScale(2, BigDecimal.ROUND_DOWN).floatValue();
 	}
 	
 	/**
