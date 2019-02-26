1985305c8e2acf3c72df2a7743263d616626be8c
mabin <jansan_godsend@sina.com>
2019-02-26 15:09:30 +0800

    代理商信息管理

---
+++ ShareAirCleanerSystem/AirCleaner/src/main/java/com/qt/air/cleaner/market/vo/generic/AgentView.java
@@ -0,0 +1,8 @@
+package com.qt.air.cleaner.market.vo.generic;
+
+import java.io.Serializable;
+
+public class AgentView implements Serializable {
+	private static final long serialVersionUID = 8942711772125719926L;
+
+}
