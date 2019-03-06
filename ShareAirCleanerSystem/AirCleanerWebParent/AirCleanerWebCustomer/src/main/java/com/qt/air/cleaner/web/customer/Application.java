a094614323981ac6d71202626f2fc8e0e1148496
mabin <jansan_godsend@sina.com>
2019-03-06 18:40:23 +0800

    查询设备状态超时

--- ShareAirCleanerSystem/AirCleanerWebParent/AirCleanerWebCustomer/src/main/java/com/qt/air/cleaner/web/customer/Application.java
+++ ShareAirCleanerSystem/AirCleanerWebParent/AirCleanerWebCustomer/src/main/java/com/qt/air/cleaner/web/customer/Application.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import feign.Retryer;
 
 @SpringBootApplication
 @EnableEurekaClient
