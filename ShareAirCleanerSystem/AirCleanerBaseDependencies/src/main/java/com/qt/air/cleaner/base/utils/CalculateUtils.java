b110f933c1389baa25076ea904c3fdb7a23e40d2
mabin <jansan_godsend@sina.com>
2019-03-27 04:10:54 +0800

    分润计算方法追加

--- ShareAirCleanerSystem/AirCleanerBaseDependencies/src/main/java/com/qt/air/cleaner/base/utils/CalculateUtils.java
+++ ShareAirCleanerSystem/AirCleanerBaseDependencies/src/main/java/com/qt/air/cleaner/base/utils/CalculateUtils.java
@@ -28,4 +28,62 @@ public class CalculateUtils {
 		return b1.subtract(b2).setScale(2, BigDecimal.ROUND_DOWN).floatValue();
 	}
 	
+	/**
+	 * 投资商分润金额计算
+	 * @param totalAmount
+	 * @param proportion
+	 * @return
+	 */
+	public static Float getInvestorAmount(Float totalAmount,Integer proportion,Integer costTime,Float free){
+		BigDecimal b1 = new BigDecimal(Float.toString(totalAmount));
+		Float result = b1.multiply(new BigDecimal(Integer.toString(proportion))).divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_DOWN).floatValue();
+		BigDecimal freeBigDecimal = new BigDecimal(Float.toString(free));
+		free = freeBigDecimal.multiply(new BigDecimal(costTime)).setScale(2, BigDecimal.ROUND_DOWN).floatValue();
+		// 当计算金额大于耗材费用 才扣除耗材费用
+		if (result>free) {
+			result = new BigDecimal(Float.toString(result)).subtract(new BigDecimal(Float.toString(free))).setScale(2, BigDecimal.ROUND_DOWN).floatValue();
+		}
+		return result;
+	}
+	
+	/**
+	 * 公司分润金额计算
+	 * 
+	 * @param totalAmount
+	 * @param proportion
+	 * @return
+	 */
+	public static Float getCompanyAmount(Float totalAmount,Integer proportion) {
+		return getShareAmount(totalAmount, proportion);
+	}
+	
+	/**
+	 * 投资商分润金额计算
+	 * 
+	 * @param totalAmount
+	 * @param proportion
+	 * @return
+	 */
+	public static Float getTraderAmount(Float totalAmount,Integer proportion) {
+		return getShareAmount(totalAmount, proportion);
+	}
+	
+	
+	/**
+	 * 代理商分润金额计算
+	 * 
+	 * @param totalAmount
+	 * @param proportion
+	 * @return
+	 */
+	public static Float getAgentAmount(Float totalAmount,Integer proportion) {
+		return getShareAmount(totalAmount, proportion);
+	}
+	
+	
+	private static Float getShareAmount(Float totalAmount,Integer proportion) {
+		return new BigDecimal(Float.toString(totalAmount)).multiply(new BigDecimal(Integer.toString(proportion)))
+				.divide(new BigDecimal(100)).setScale(2, BigDecimal.ROUND_DOWN).floatValue();
+	}
+	
 }
