43f8d460c6f6938d7a27d5709f7b18d22e81d2ec
mabin <mabin@localhost>
2019-04-12 11:01:30 +0800

    指定href属性值target，在iframe里面显示

--- ShareAirCleanerSystem/AirCleaner/src/main/resources/static/js/market/generic/queryDeviceMonitorIndex.js
+++ ShareAirCleanerSystem/AirCleaner/src/main/resources/static/js/market/generic/queryDeviceMonitorIndex.js
@@ -1,17 +1,16 @@
 layui.use(['tree'], function(){
     layui.tree({
         elem: '#demo' //传入元素选择器
+        ,target:'bodyFrame'
         ,nodes: [{ //节点
             name: '设备监控'
             ,spread:true
             ,children: [{
                 name: '频率使用率监控',
-                href:'/welcome',
-                target:'bodyFrame'
+                href:'/welcome'
             },{
                 name: '设备滤芯监控',
-                href:'/welcome',
-                target:'bodyFrame'
+                href:'/welcome'
             }]
         }]
     });
