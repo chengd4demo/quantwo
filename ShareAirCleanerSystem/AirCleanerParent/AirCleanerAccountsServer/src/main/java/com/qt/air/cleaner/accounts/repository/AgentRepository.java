6cba7ada538f056fd66f223036eb3c883fed8524
kaedeliu <88526743@qq.com>
2019-05-07 11:23:51 +0800

    add

---
+++ ShareAirCleanerSystem/AirCleanerParent/AirCleanerAccountsServer/src/main/java/com/qt/air/cleaner/accounts/repository/AgentRepository.java
@@ -0,0 +1,14 @@
+package com.qt.air.cleaner.accounts.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.qt.air.cleaner.accounts.domain.Agent;
+
+@Repository
+public interface AgentRepository extends JpaRepository<Agent, String> {
+	Agent findByWeixinAndRemoved(String weixin,Boolean removed);
+	Agent findByIdentificationNumberAndPhoneNumberAndRemoved(String identificationNumber,String phoneNumber,Boolean removed);
+	Agent findByWeixinAndPhoneNumberAndRemoved(String weixin,String phoneNumber,Boolean removed);
+	Agent findByWeixin(String weixin);
+}
